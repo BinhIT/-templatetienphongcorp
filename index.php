a3d6f59262a75c4f0adf4326aabff3b9f83882bc
BinhIT <nbinh6538@gmail.com>
2020-12-15 14:37:19 +0700

    fist commit

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-    header("Location: ./site/gioi-thieu.php");
+require './global.php';
+if (exist_param("gioi-thieu")) {
+    header("location:site/gioi-thieu.php ");
+} else if (exist_param("lien-he")) {
+    header("location:site/contact.html ");
+} else if (exist_param("gop-y")) {
+    header("location:site/contact.html ");
+} else if (exist_param("hoi-dap")) {
+    header("location:site/contact.html ");
+} else {
+    header("location:site/index.html ");
+}
+
+require '../layout.php';
